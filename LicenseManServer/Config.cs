d8c11193b5bab9626c0a830553104782940cfa39
Jamie Hankins <jamiehankin@gmail.com>
2015-02-02 01:52:43 +0000

    Add some more core stuff, NLog and a Config File

--- LicenseManServer/Config.cs
+++ LicenseManServer/Config.cs
@@ -1,13 +1,16 @@
-﻿using System;
+﻿using Newtonsoft.Json;
+using System;
 using System.Collections.Generic;
+using System.IO;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
 
 namespace LicenseManServer
 {
-    class Config
+    [Serializable]
+    public class Config
     {
-
+        public string ApplicationName = "";
     }
 }
--- LicenseManServer/LicenseManServer.cs
+++ LicenseManServer/LicenseManServer.cs
@@ -1,5 +1,8 @@
-﻿using System;
+﻿using Newtonsoft.Json;
+using NLog;
+using System;
 using System.Collections.Generic;
+using System.IO;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
@@ -8,9 +11,31 @@ namespace LicenseManServer
 {
     class LicenseManServer
     {
+        private Config Config;
+        private Logger Logger;
+
+        public LicenseManServer()
+        {
+            Logger = LogManager.GetLogger("LicenseManServer");
+
+            if (File.Exists("LicenseMan.config"))
+            {
+                Config = JsonConvert.DeserializeObject<Config>(File.ReadAllText("LicenseMan.config"));
+            }
+            else
+            {
+                Logger.Warn("Writing out config file 'LicenseMan.config' - please fill it in");
+                Config = new Config();
+
+                var Json = JsonConvert.SerializeObject(Config);
+                File.WriteAllText("LicenseMan.config", Json);
+                Environment.Exit(-1);
+            }
+        }
+
         public void Start()
         {
-
+            Logger.Info("Starting LicenseManServer for: {0}", Config.ApplicationName);
         }
     }
 }
